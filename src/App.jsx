8d19a348a5d2bdebe866890d6acc662f8680f899
Elliott Parker <kparker1848@gmail.com>
2022-01-20 17:37:54 -0600

    mobile styling fix

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,13 @@ const App = () => {
                 </div>
                 <h1 className="text-center m-5">Studio Ghibli API Lab</h1>
                 <div className="container d-flex flex-wrap justify-content-center">
-                    <div className="col-6 d-flex justify-content-evenly">
-                        <button type="button" className="btn btn-info text-light" onClick={handleFilmButtonClick}>Films</button>
-                        <button type="button" className="btn btn-info text-light" onClick={handlePeopleButtonClick}>Characters</button>
+                    <div className="col-6-md d-flex justify-content-evenly">
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handleFilmButtonClick}>Films</button>
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handlePeopleButtonClick}>Characters</button>
                     </div>
                     <div className="col-12 d-flex m-5 justify-content-center">
 
-                        <div className="card shadow p-3 mb-5 bg-body rounded col-4">
+                        <div className="card shadow p-3 mb-5 bg-body rounded col-lg-4">
                             <div className="card-body">
                                 <h4 className="m-5 text-center">Welcome to the API Lab!</h4>
                             </div>
@@ -57,18 +57,18 @@ const App = () => {
         return (
             <>
                 <div className="col-12 d-flex flex-wrap justify-content-center">
-                    <img className="col-4" src="https://data.whicdn.com/images/350711764/original.gif" alt="Birds flying" />
+                    <img className="col-4-md" src="https://data.whicdn.com/images/350711764/original.gif" alt="Birds flying" />
                 </div>
                 <h1 className="text-center m-5">Studio Ghibli API Lab</h1>
                 <div className="container d-flex flex-wrap justify-content-center">
-                    <div className="col-6 d-flex justify-content-evenly">
-                        <button type="button" className="btn btn-info text-light" onClick={handleFilmButtonClick}>Films</button>
-                        <button type="button" className="btn btn-info text-light" onClick={handlePeopleButtonClick}>Characters</button>
+                    <div className="col-6-md d-flex justify-content-evenly">
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handleFilmButtonClick}>Films</button>
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handlePeopleButtonClick}>Characters</button>
                     </div>
                     <div >
                         <ul className="col-12 d-flex flex-wrap justify-content-evenly">
                             {filmList.map((films) => (
-                                <div key={`film-id-${films.id}`} className="card shadow p-3 mb-5 bg-body rounded col-4 m-4">
+                                <div key={`film-id-${films.id}`} className="card shadow p-3 mb-5 bg-body rounded col-md-4 m-4">
                                     <img src={films.movie_banner} alt="..." className="card-img-top" />
                                     <div className="card-body">
                                         <h4 className="card-title text-center text-decoration-underline">{films.title}</h4>
@@ -88,18 +88,18 @@ const App = () => {
         return (
             <>
                 <div className="col-12 d-flex flex-wrap justify-content-center">
-                    <img className="col-4" src="https://64.media.tumblr.com/a2c7267b99f34f1e9fbe2ca65f801ae1/3e6a8ea646afc9c3-7e/s540x810/b8533fb27d00270ad68233695f22c48f62a96f14.gifv" alt="Animal ears in tall grass" />
+                    <img className="col-4-md" src="https://64.media.tumblr.com/a2c7267b99f34f1e9fbe2ca65f801ae1/3e6a8ea646afc9c3-7e/s540x810/b8533fb27d00270ad68233695f22c48f62a96f14.gifv" alt="Animal ears in tall grass" />
                 </div>
                 <h1 className="text-center m-5">Studio Ghibli</h1>
                 <div className="container d-flex flex-wrap justify-content-center">
-                    <div className="col-6 d-flex justify-content-evenly">
-                        <button type="button" className="btn btn-info text-light" onClick={handleFilmButtonClick}>Films</button>
-                        <button type="button" className="btn btn-info text-light" onClick={handlePeopleButtonClick}>Characters</button>
+                    <div className="col-6-md d-flex justify-content-evenly">
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handleFilmButtonClick}>Films</button>
+                        <button type="button" className="btn btn-info text-light m-2" onClick={handlePeopleButtonClick}>Characters</button>
                     </div>
                     <div>
                         <ul className="col-12 d-flex flex-wrap justify-content-evenly">
                             {peopleList.map((people) => (
-                                <div key={`people-id-${people.id}`} className="card shadow p-3 mb-5 bg-body rounded col-4 m-4">
+                                <div key={`people-id-${people.id}`} className="card shadow p-3 mb-5 bg-body rounded col-4-md m-4">
                                     <div className="card-body">
                                         <h4 className="card-title text-center text-decoration-underline">{people.name}</h4>
                                         <p className="card-text">Age: {people.age}</p>
